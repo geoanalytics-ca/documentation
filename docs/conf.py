f981315a114ff9be478df064450e5487dabd34e2
Benjamin Smith <benjaminsmith@Benjamins-MacBook-Air.local>
2022-06-08 11:51:43 -0700

    stashing edits

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '2022'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-needs_sphinx = '3.0'
+needs_sphinx = '4.0'
 
 extensions = [
     "nbsphinx",
